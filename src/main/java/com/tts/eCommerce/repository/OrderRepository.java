e22cdd3cb523cfe5dd10cf4d00910b60087a9b3e
ireneonianwa <ireneonianwa@Irenes-MacBook-Pro.local>
2020-07-07 12:15:02 -0700

    updated repositories

--- src/main/java/com/tts/eCommerce/repository/OrderRepository.java
+++ src/main/java/com/tts/eCommerce/repository/OrderRepository.java
@@ -1,9 +1,14 @@
 package com.tts.eCommerce.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tts.eCommerce.model.Order;
 
 @Repository
 public interface OrderRepository 
 	extends CrudRepository<Order, Long> {
+	
+	public Order findByOrder(String order);
 
 }
--- src/main/java/com/tts/eCommerce/repository/ProductRepository.java
+++ src/main/java/com/tts/eCommerce/repository/ProductRepository.java
@@ -1,9 +1,13 @@
 package com.tts.eCommerce.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tts.eCommerce.model.Product;
 
 @Repository
 public interface ProductRepository 
 	extends CrudRepository<Product, Long> {
 
+	public Product findByProduct(String product);
 }
