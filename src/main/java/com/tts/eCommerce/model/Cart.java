be5b69dfbd1dbbebe1fa60feeff5ae908606338a
Michael Oluh <michaeloluh@Michaels-MacBook-Pro.local>
2020-07-07 21:41:23 -0500

    updated cart model

--- src/main/java/com/tts/eCommerce/model/Cart.java
+++ src/main/java/com/tts/eCommerce/model/Cart.java
@@ -1,5 +1,38 @@
 package com.tts.eCommerce.model;
 
-public class Cart {
+import java.util.HashMap;
 
+public class Cart {
+	
+	private HashMap<Product, Integer> lineitem;
+	
+	private int Quantity;
+
+	public Cart() {
+		
+	}
+
+	public Cart(HashMap<Product, Integer> lineitem, int quantity) {
+		this.lineitem = lineitem;
+		Quantity = quantity;
+	}
+
+	public HashMap<Product, Integer> getLineitem() {
+		return lineitem;
+	}
+
+	public void setLineitem(HashMap<Product, Integer> lineitem) {
+		this.lineitem = lineitem;
+	}
+
+	public int getQuantity() {
+		return Quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		Quantity = quantity;
+	}
+	
+	
+	
 }
