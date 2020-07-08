7152321e6b5c01776a80a7a14581326e12937473
Michael Oluh <michaeloluh@Michaels-MacBook-Pro.local>
2020-07-08 06:57:30 -0500

    set up the get and find methods

--- src/main/java/com/tts/eCommerce/service/OrderService.java
+++ src/main/java/com/tts/eCommerce/service/OrderService.java
@@ -1,5 +1,35 @@
 package com.tts.eCommerce.service;
 
-public class OrderService {
+import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tts.eCommerce.model.Order;
+import com.tts.eCommerce.repository.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	public Order findByOrder(String order) {
+		return orderRepository.findByOrder(order);
+	}
+	
+    public Iterable<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+	
+    
+    public Order create(Order order) {
+        order.setCreatedAt(new Date());
+        return this.orderRepository.save(order);
+    }
+ 
+   
+    public void update(Order order) {
+        this.orderRepository.save(order);
+    }
 }
