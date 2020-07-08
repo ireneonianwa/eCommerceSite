5c10e6a38c7acad0316942bd0c802d35340460f1
Michael Oluh <michaeloluh@Michaels-MacBook-Pro.local>
2020-07-08 06:30:57 -0500

    first commit containing get and find methods to obtain products

--- src/main/java/com/tts/eCommerce/service/ProductService.java
+++ src/main/java/com/tts/eCommerce/service/ProductService.java
@@ -1,5 +1,33 @@
 package com.tts.eCommerce.service;
 
-public class ProductService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+
+import com.tts.eCommerce.model.Product;
+import com.tts.eCommerce.repository.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	public Product findByProduct(String product) {
+		return productRepository.findByProduct(product);
+	}
+	
+	public Iterable<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+ 
+    public Product getProduct(Long Id) {
+        return productRepository.getProductById(Id);
+//          .findById(id)
+//          .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+    }
+ 
+    public void save(Product product) {
+         productRepository.save(product);
+    } 
 }
