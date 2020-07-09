424bd22ae47755a89a2678e49105452c81d0f898
Michael Oluh <michaeloluh@Michaels-MacBook-Pro.local>
2020-07-08 19:19:56 -0500

    modified cartController

--- src/main/java/com/tts/eCommerce/controller/CartController.java
+++ src/main/java/com/tts/eCommerce/controller/CartController.java
@@ -1,9 +1,13 @@
+//I commented out some of your code when discussiing these pages with David, he also had some methods he wanted to take from what he initially posted//
+
 package com.tts.eCommerce.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -11,73 +15,63 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.tts.eCommerce.model.Cart;
 import com.tts.eCommerce.model.Product;
 import com.tts.eCommerce.model.User;
+import com.tts.eCommerce.service.CartService;
 import com.tts.eCommerce.service.ProductService;
 import com.tts.eCommerce.service.UserService;
 
 @Controller
 public class CartController {
-	
+
 	@Autowired
 	private ProductService productService;
 	@Autowired
 	private UserService userService;
-	
-	@ModelAttribute("loggedInUser")
-	public User loggedInUser() {
-		return userService.getLoggedInUser();
-	}
-	
-	@ModelAttribute("cart")
-	public Map<Product, Integer> cart() {
-		User user = loggedInUser();
-		if(user == null) 
-			return null;
-		System.out.println("Get cart");
-		return user.getCart();
-	}
-	
-	@ModelAttribute("list")
-	public List<Double> list(){
-		
-	}
-	
+
+	@Autowired
+	private CartService cartService;
+
+//	
+//	@ModelAttribute("cart")
+//	public Map<Product, Integer> cart() {
+//		User user = loggedInUser();
+//		if(user == null) 
+//			return null;
+//		System.out.println("Get cart");
+//		return user.getCart();
+//	}
+//	
+//	@ModelAttribute("list")
+//	public List<Double> list(){
+//		
+//	}
+
 	@GetMapping("/cart")
 	public String showCart() {
-	  return "storefront/cart";
+		return "storefront/cart";
 	}
-	 
+
 	@PostMapping("/cart")
-	public String addToCart(@RequestParam long id) {
-	  Product product = productService.findById(id);
-	  setQuantity(product, cart().getOrDefault(product, 0) + 1);
-	  return "storefront/cart";
+	public String addToCart(@RequestParam long productId, Cart cart, Integer quantity, Model model) {
+		cartService.addProductToCart(cart, productId, quantity);
+		model.addAttribute("cart", cart);
+		return "storefront/cart";
 	}
 
 	@PatchMapping("/cart")
 	public String updateQuantities(@RequestParam Long[] productId, @RequestParam Integer[] quantity) {
-	  for(int i = 0; i < productId.length; i++) {
-	      Product product = productService.findById(productId[i]);
-	      setQuantity(product, quantity[i]);
-	  }
-	  return "storefront/cart";
+		for (int i = 0; i < productId.length; i++) {
+			Product product = productService.findById(productId[i]);
+		}
+		return "storefront/cart";
 	}
 
 	@DeleteMapping("/cart")
 	public String removeFromCart(@RequestParam long id) {
-	  Product product = productService.findById(id);
-	  setQuantity(product, 0);
-	  return "storefront/cart";
-	}
 
-	private void setQuantity(Product p, int quantity) {
-	  if(quantity > 0){
-	    cart().put(p, quantity);
-	  } else {
-	    cart().remove(p);
-	  }
-	  userService.updateCart(cart());
+		return "storefront/cart";
 	}
 
 }
