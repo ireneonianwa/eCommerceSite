5fbd758d3708884cf4fd658349790ffa176315b2
Michael Oluh <michaeloluh@Michaels-MacBook-Pro.local>
2020-07-08 19:16:34 -0500

    created a cartservice

---
+++ src/main/java/com/tts/eCommerce/service/CartService.java
@@ -0,0 +1,27 @@
+package com.tts.eCommerce.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tts.eCommerce.model.Cart;
+import com.tts.eCommerce.model.Product;
+
+@Service
+public class CartService {
+	@Autowired
+	private ProductService productService;
+	
+	public Cart addProductToCart(Cart cart, Long productId, Integer quantity) {
+		Product productToAdd = productService.findById(productId);
+		HashMap<Product, Integer> newLineItem = new HashMap<>();
+		newLineItem.put(productToAdd, quantity);
+		List<HashMap<Product, Integer>> cartLineItems = cart.getLineitems();
+		cartLineItems.add(newLineItem);
+		cart.setLineitems(cartLineItems);
+		return cart;
+	}
+}
